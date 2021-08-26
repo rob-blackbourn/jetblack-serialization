5e2798aba9343b0979e8e067eaa5a84b1553b86b
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-08-26 09:28:01 +0100

    Generalised custom annotation functions

--- jetblack_serialization/custom_annotations.py
+++ jetblack_serialization/custom_annotations.py
@@ -1,11 +1,12 @@
 """Custom annotations"""
 
 from abc import ABCMeta
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Type, TypeVar
 
 import jetblack_serialization.typing_inspect_ex as typing_inspect
 from .types import Annotation
 
+T = TypeVar('T')
 
 class SerializationAnnotation(metaclass=ABCMeta):
     """The base serialization annotation class"""
@@ -14,6 +15,26 @@ class DefaultAnnotation:
 
     def __init__(self, value: Any) -> None:
         self.value = value
+
+def is_any_annotation_of_type(annotation: Annotation, tp: Type[Any]) -> bool:
+    if not typing_inspect.is_annotated_type(annotation):
+        return False
+    for item in typing_inspect.get_metadata(annotation):
+        if issubclass(type(item), tp):
+            return True
+    return False
+
+def get_all_annotations_of_type(
+        annotation: Annotation,
+        tp: Type[T]
+) -> Tuple[Annotation, List[T]]:
+    type_annotation = typing_inspect.get_origin(annotation)
+    serialization_annotations = [
+        serialization_annotation
+        for serialization_annotation in typing_inspect.get_metadata(annotation)
+        if issubclass(type(serialization_annotation), tp)
+    ]
+    return type_annotation, serialization_annotations
 
 def is_any_serialization_annotation(annotation: Annotation) -> bool:
     """Determine if the annotation is of type Annotation[T, SerializationAnnotation]
@@ -25,12 +46,7 @@ def is_any_serialization_annotation(annotation: Annotation) -> bool:
         bool: True if the annotation is of type
             Annotation[T, SerializationAnnotation], otherwise False
     """
-    if not typing_inspect.is_annotated_type(annotation):
-        return False
-    for item in typing_inspect.get_metadata(annotation):
-        if issubclass(type(item), SerializationAnnotation):
-            return True
-    return False
+    return is_any_annotation_of_type(annotation, SerializationAnnotation)
 
 
 def get_all_serialization_annotations(
@@ -45,10 +61,14 @@ def get_all_serialization_annotations(
         Tuple[Annotation, List[SerializationAnnotation]]: The type and the
             serialization annotationa
     """
-    type_annotation = typing_inspect.get_origin(annotation)
-    serialization_annotations = [
-        serialization_annotation
-        for serialization_annotation in typing_inspect.get_metadata(annotation)
-        if issubclass(type(serialization_annotation), SerializationAnnotation)
-    ]
-    return type_annotation, serialization_annotations
+    return get_all_annotations_of_type(annotation, SerializationAnnotation)
+
+
+def is_any_default_annotation(annotation: Annotation) -> bool:
+    return is_any_annotation_of_type(annotation, DefaultAnnotation)
+
+
+def get_all_default_annotations(
+        annotation: Annotation
+) -> Tuple[Annotation, List[DefaultAnnotation]]:
+    return get_all_annotations_of_type(annotation, DefaultAnnotation)
