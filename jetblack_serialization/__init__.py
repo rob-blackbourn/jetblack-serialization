91c185fb5cf0ff408305470ec7f69217d83c133c
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-08-02 08:44:58 +0100

    tidy

--- jetblack_serialization/__init__.py
+++ jetblack_serialization/__init__.py
@@ -1,7 +1,17 @@
 """Serialization"""
 
+from .config import (
+    VALUE_DESERIALIZERS,
+    VALUE_SERIALIZERS,
+    ValueDeserializer,
+    ValueSerializer
+)
 from .custom_annotations import DefaultValue
 
 __all__ = [
+    'VALUE_DESERIALIZERS',
+    'VALUE_SERIALIZERS',
+    'ValueDeserializer',
+    'ValueSerializer',
     'DefaultValue'
 ]
